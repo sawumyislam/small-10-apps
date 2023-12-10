33650b1ab17e1f18bc504a85b79d409820ec690f
sawumyislam <sawumy.ais@gmail.com>
2023-12-11 00:30:21 +0600

    try

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,10 +1,34 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./style.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="calculator-grid">
+        {/* output */}
+        <div className="output">
+          <div className="previous-operand">INPUT</div>
+          <div className="current-operand">OUTPUT</div>
+        </div>
+        {/* input */}
+        <button className="span-two">AC</button>
+        <button>DEL</button>
+        <button>/</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button className="span-two">=</button>
+      </div>
     </div>
   );
 }
